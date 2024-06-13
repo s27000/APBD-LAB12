b9850148201dc7bdfd7a983217885a2b33ee688b
PC <s27000@pjwstk.edu.pl>
2024-06-13 18:27:36 +0200

    Add a Model

--- ProjektLAB12/TutorialMVC/Program.cs
+++ ProjektLAB12/TutorialMVC/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TutorialMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TutorialMVCContext") ?? throw new InvalidOperationException("Connection string 'TutorialMVCContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
