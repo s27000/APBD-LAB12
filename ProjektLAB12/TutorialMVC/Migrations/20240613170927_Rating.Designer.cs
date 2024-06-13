30ce9d3c2fb121bdb6acd8db9cf714c204402279
PC <s27000@pjwstk.edu.pl>
2024-06-13 19:23:33 +0200

    add validation

--- ProjektLAB12/TutorialMVC/Migrations/20240613170927_Rating.Designer.cs
+++ ProjektLAB12/TutorialMVC/Migrations/20240613170927_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Data;
 
@@ -11,9 +12,11 @@ using MvcMovie.Data;
 namespace TutorialMVC.Migrations
 {
     [DbContext(typeof(TutorialMVCContext))]
-    partial class TutorialMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20240613170927_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
